47426d827892dae205f164506ddc309911be3423
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-05-14 14:41:16 +0200

    Added vue

---
+++ resources/views/partials/vue_component.blade.php
@@ -0,0 +1,5 @@
+@extends('temeplates.base')
+
+@section('pageMain')
+    <example-component></example-component>
+@endsection
--- resources/views/temeplates/base.blade.php
+++ resources/views/temeplates/base.blade.php
@@ -13,5 +13,9 @@
     @include('partials.header')
     @yield('pageMain')
     @include('partials.footer')
+
+    <script src="/js/manifest.js"></script>
+    <script src="/js/vendor.js"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>
