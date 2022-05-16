29e75838b0a72cd43469cfbf0872b7a97575f545
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-05-16 17:31:52 +0200

    Added header logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,9 @@
     <nav class="navbar fixed-top navbar-expand navbar-dark bg-dark">
         <div class="container-md">
             
-            <a class="navbar-brand text-primary" href="{{ route('home') }}">DC Comics</a>
+            <a class="navbar-brand text-primary img-container m-0 p-0" href="{{ route('home') }}">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="">
+            </a>
             
             <a class="btn btn-outline-primary" href="{{ route('comics.create') }}">Add New Comic</a>
             
